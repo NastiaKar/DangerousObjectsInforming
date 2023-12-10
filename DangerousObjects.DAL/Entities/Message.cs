070443be78734d12b9410e98f2f54f4dee2cacc5
IK-12 Настя Карпюк <nastia.karpyuk@outlook.com>
2023-12-11 00:56:27 +0200

    removed receiver

--- DangerousObjects.DAL/Entities/Message.cs
+++ DangerousObjects.DAL/Entities/Message.cs
@@ -10,13 +10,10 @@ public class Message : BaseEntity
     public string Content { get; set; } = null!;
     public DateTime DateSent { get; set; }
     public int SenderId { get; set; }
-    public int ReceiverId { get; set; }
     public int ImportanceId { get; set; }
     public int DangerousObjectId { get; set; }
     [ForeignKey(nameof(SenderId))]
     public User Sender { get; set; } = null!;
-    [ForeignKey(nameof(ReceiverId))]
-    public User Receiver { get; set; } = null!;
     [ForeignKey(nameof(ImportanceId))]
     public Importance Importance { get; set; }
     [ForeignKey(nameof(DangerousObjectId))]
