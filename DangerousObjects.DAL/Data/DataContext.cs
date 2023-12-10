37f9b0bf5eb3c3f719296a56a7430550c16d838e
IK-12 Настя Карпюк <nastia.karpyuk@outlook.com>
2023-12-11 01:06:12 +0200

    defined dbcontext

---
+++ DangerousObjects.DAL/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using DangerousObjectsCommon.Enums;
+using DangerousObjectsDAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DangerousObjectsDAL.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+}
