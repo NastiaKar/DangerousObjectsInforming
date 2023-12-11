0e92beb77ba0921bbb9c222b76542735187f78fe
IK-12 Настя Карпюк <nastia.karpyuk@outlook.com>
2023-12-11 17:45:05 +0200

    added DataContextFactory and updated DataContext

--- DangerousObjects.DAL/Data/DataContext.cs
+++ DangerousObjects.DAL/Data/DataContext.cs
@@ -1,10 +1,12 @@
-﻿using DangerousObjectsCommon.Enums;
-using DangerousObjectsDAL.Entities;
+﻿using DangerousObjectsDAL.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace DangerousObjectsDAL.Data;
 
 public class DataContext : DbContext
 {
+    public DbSet<User> Users { get; set; } = null!;
+    public DbSet<DangerousObject> DangerousObjects { get; set; } = null!;
+    public DbSet<Message> Messages { get; set; } = null!;
     public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 }
---
+++ DangerousObjects.DAL/Data/DataContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace DangerousObjectsDAL.Data;
+
+public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+{
+    public DataContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+        optionsBuilder.UseSqlServer("Server=localhost;Database=DangerousObjects;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+        return new DataContext(optionsBuilder.Options);
+    }
+}
