adae31f18edf3390ab92d85b9fa0634edf336f2d
IK-12 Настя Карпюк <nastia.karpyuk@outlook.com>
2023-12-11 18:54:42 +0200

    String extensions added

---
+++ DangerousObjects.BLL/Extensions/StringExtensions.cs
@@ -0,0 +1,30 @@
+﻿using DangerousObjectsCommon.Enums;
+
+namespace DangerousObjectsBLL.Extensions;
+
+public static class StringExtensions
+{
+    public static ObjType ConvertToObjType(this string source)
+    {
+        bool parsed = Enum.TryParse(source, out ObjType objType);
+        if (!parsed)
+            throw new Exception("Type not found");
+        return objType;
+    }
+    
+    public static Position ConvertToPosition(this string source)
+    {
+        bool parsed = Enum.TryParse(source, out Position position);
+        if (!parsed)
+            throw new Exception("Position not found");
+        return position;
+    }
+    
+    public static Importance ConvertToImportance(this string source)
+    {
+        bool parsed = Enum.TryParse(source, out Importance importance);
+        if (!parsed)
+            throw new Exception("Importance not found");
+        return importance;
+    }
+}
