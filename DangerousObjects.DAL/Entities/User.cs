5e89ec80b74a4b2bd9007d52247f63f87f9d4d83
IK-12 Настя Карпюк <nastia.karpyuk@outlook.com>
2023-12-11 18:43:44 +0200

    Auth model added, User entity updates with password hash and salt

--- DangerousObjects.DAL/Entities/User.cs
+++ DangerousObjects.DAL/Entities/User.cs
@@ -8,7 +8,8 @@ public class User : BaseEntity
 {
     public string Name { get; set; } = null!;
     public string Email { get; set; } = null!;
-    public string Password { get; set; } = null!;
+    public string PasswordHash { get; set; } = String.Empty;
+    public string Salt { get; set; } = String.Empty;
     public string PhoneNumber { get; set; } = null!;
     [ForeignKey(nameof(PositionId))] 
     public Position Position { get; set; }
