2b28d495bcc65af32edacd94cf14957c07899238
IK-12 Настя Карпюк <nastia.karpyuk@outlook.com>
2023-12-28 15:47:01 +0200

    changed DisplayMessage DTO

--- DangerousObjects.Models/DTOs/Message/DisplayMessage.cs
+++ DangerousObjects.Models/DTOs/Message/DisplayMessage.cs
@@ -1,11 +1,14 @@
-﻿namespace DangerousObjectsCommon.DTOs.Message;
+﻿using DangerousObjectsCommon.DTOs.DangerousObject;
+
+namespace DangerousObjectsCommon.DTOs.Message;
 
 public class DisplayMessage
 {
     public string Title { get; set; } = null!;
     public string Content { get; set; } = null!;
-    public int ImportanceId { get; set; }
+    public string Importance { get; set; } = null!;
     public int DangerousObjectId { get; set; }
+    public DisplayDangerousObject DangerousObject { get; set; } = null!;
     public int SenderId { get; set; }
     public DateTime DateSent { get; set; }
 }
