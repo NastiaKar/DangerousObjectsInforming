f17f95e4a74befd5c09bd0a3e2b687d7cbef20d1
IK-12 Настя Карпюк <nastia.karpyuk@outlook.com>
2024-01-12 05:39:38 +0200

    Revert "updated libraries"
    This reverts commit ca81001d85b4c92cc0c424f2fc110b8d7f9785db.

--- DangerousObjects.WebAPI/Program.cs
+++ DangerousObjects.WebAPI/Program.cs
@@ -1,16 +1,9 @@
 using System.Reflection;
 using DangerousObjectsBLL.Configure;
-using DangerousObjectsBLL.Services;
-using DangerousObjectsBLL.Services.Interfaces;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using DangerousObjectsDAL.Data;
-using DangerousObjectsDAL.Repositories;
-using DangerousObjectsDAL.Repositories.Interfaces;
-using DangerousObjectsInforming.Extensions;
-using DangerousObjectsInforming.Filters;
-using DangerousObjectsInforming.Validators;
-using FluentValidation;
-using FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,28 +12,10 @@ builder.Services.Configure<JwtConfig>(config => config.Secret = builder.Configur
 // Add services to the container.
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
-builder.Services.AddScoped<IDangerousObjectService, DangerousObjectService>();
-builder.Services.AddScoped<IMessageService, MessageService>();
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
-builder.Services.AddScoped<IUserRepo, UserRepo>();
-builder.Services.AddScoped<IDangerousObjectRepo, DangerousObjectRepo>();
-builder.Services.AddScoped<IMessageRepo, MessageRepo>();
-builder.Services.AddValidatorsFromAssemblyContaining<CreateDangerousObjectValidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<CreateMessageValidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<UpdateDangerousObjectValidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<UpdateMessageValidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<UserLoginRequestValidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<UserRegisterRequestValidator>();
-
-builder.Services.AddControllers(config =>
-    { config.Filters.Add<CustomExceptionFilterAttribute>(); });
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddJwtAuthentication(builder.Configuration);
 
 var app = builder.Build();
 
