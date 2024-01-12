2c8dbda7fea9f28494f8d4b9c6b54a441de793d5
IK-12 Настя Карпюк <nastia.karpyuk@outlook.com>
2024-01-12 03:26:07 +0200

    added filters

---
+++ DangerousObjects.WebAPI/Filters/CustomExceptionFilterAttribute.cs
@@ -0,0 +1,34 @@
+﻿using DangerousObjectsCommon.Errors;
+using DangerousObjectsCommon.Exceptions.Base;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace DangerousObjectsInforming.Filters;
+
+public class CustomExceptionFilterAttribute : ExceptionFilterAttribute
+{
+    public override void OnException(ExceptionContext context)
+    {
+        var ex = context.Exception;
+        var message = ex.Message;
+
+        IActionResult result = ex switch
+        {
+            BaseCustomException baseEx => new BadRequestObjectResult(new ErrorResponse
+            {
+                Errors = new List<ErrorModel>
+                    { new ErrorModel { CauseOfError = baseEx.CauseOfError, Message = message } }
+            }),
+            _ => new ObjectResult(new ErrorResponse
+            {
+                Errors = new List<ErrorModel> 
+                    {new ErrorModel {CauseOfError = "General Error", Message = message}}
+            })
+            {
+                StatusCode = 500
+            }
+        };
+        context.ExceptionHandled = true;
+        context.Result = result;
+    }
+}
