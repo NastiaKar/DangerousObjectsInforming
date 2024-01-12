d663a383627ee82b941923d56fce0051f60b116c
IK-12 Настя Карпюк <nastia.karpyuk@outlook.com>
2024-01-12 02:16:08 +0200

    added service interfaces for dangerous object and messafe

---
+++ DangerousObjects.BLL/Services/Interfaces/IDangerousObjectService.cs
@@ -0,0 +1,12 @@
+﻿using DangerousObjectsCommon.DTOs.DangerousObject;
+
+namespace DangerousObjectsBLL.Services.Interfaces;
+
+public interface IDangerousObjectService
+{
+    Task<IEnumerable<DisplayDangerousObject>> GetAll();
+    Task<DisplayDangerousObject> GetById(int id);
+    Task<DisplayDangerousObject> Create(CreateDangerousObject createDangerousObject);
+    Task<DisplayDangerousObject> Update(int id, UpdateDangerousObject updateDangerousObject);
+    Task Delete(int id);
+}
---
+++ DangerousObjects.BLL/Services/Interfaces/IMessageService.cs
@@ -0,0 +1,12 @@
+﻿using DangerousObjectsCommon.DTOs.Message;
+
+namespace DangerousObjectsBLL.Services.Interfaces;
+
+public interface IMessageService
+{
+    Task<IEnumerable<DisplayMessage>> GetAll();
+    Task<DisplayMessage> GetById(int id);
+    Task<DisplayMessage> Create(CreateMessage message);
+    Task<DisplayMessage> Update(int id, UpdateMessage message);
+    Task Delete(int id);
+}
