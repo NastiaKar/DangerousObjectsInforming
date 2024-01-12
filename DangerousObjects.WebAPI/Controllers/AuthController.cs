08edb9c552ff609909f504258611936611ae784c
IK-12 Настя Карпюк <nastia.karpyuk@outlook.com>
2024-01-12 05:56:51 +0200

    added AuthController

---
+++ DangerousObjects.WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using DangerousObjectsBLL.Services.Interfaces;
+using DangerousObjectsCommon.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DangerousObjectsInforming.Controllers;
+
+[ApiController]
+[Microsoft.AspNetCore.Components.Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _service;
+
+    public AuthController(IAuthService service)
+    {
+        _service = service;
+    }
+
+    [HttpPost("Register")]
+    public async Task<IActionResult> Register([FromBody] UserRegisterRequest request)
+    {
+        return Ok(await _service.RegisterAsync(request));
+    }
+
+    [HttpPost("Login")]
+    public async Task<IActionResult> Login([FromBody] UserLoginRequest request)
+    {
+        return Ok(await _service.LoginAsync(request));
+    }
+}
