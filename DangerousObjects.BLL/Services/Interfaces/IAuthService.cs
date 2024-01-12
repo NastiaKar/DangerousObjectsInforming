3aee5812f2867de2fc7e23a4009bb6198780263c
IK-12 Настя Карпюк <nastia.karpyuk@outlook.com>
2024-01-12 02:16:51 +0200

    added IAuthService

---
+++ DangerousObjects.BLL/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using DangerousObjectsCommon.Auth;
+
+namespace DangerousObjectsBLL.Services.Interfaces;
+
+public interface IAuthService
+{
+    Task<AuthResult> RegisterAsync(UserRegisterRequest request);
+    Task<AuthResult> LoginAsync(UserLoginRequest request);
+}
