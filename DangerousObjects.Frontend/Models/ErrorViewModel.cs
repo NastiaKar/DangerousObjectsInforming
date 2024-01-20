b830d6287e71511894c25f2e221b47e976373a18
IK-12 Настя Карпюк <nastia.karpyuk@outlook.com>
2024-01-20 22:30:26 +0200

    fix

--- DangerousObjects.Frontend/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace DangerousObjects.Frontend.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
